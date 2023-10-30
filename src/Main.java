82da71982f28a081b245f5f1525222ef592ecda1
admin-rez <kraken_1111@mail.ru>
2023-10-30 15:20:42 +0300

    Домашка работает!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         String firstName = "Ivanov";
         String middleName = "Ivan";
         String lastName = "Ivanovich";
