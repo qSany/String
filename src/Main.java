c4f06cb8c766babfd5d6caa719c7481285a80385
admin-rez <kraken_1111@mail.ru>
2023-10-30 15:16:55 +0300

    Домашка работает!!!

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        System.out.println("Ф. И. О. сотрудника - " + firstName + " " + middleName + " " + lastName);
 
+        System.out.println("Задача 2");
+        String fullName = "IVANOV IVAN IVANOVICH";
+        System.out.printf("Данные Ф.И.О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
 
+        System.out.println();
+        System.out.println("Задача 3");
+        String fullName1 = "Иванов Семён Семёнович";
+        fullName1 = fullName1.replaceAll("ё", "е");
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName1);
 
-        System.out.println("Hello world!");
+        System.out.println();
+        System.out.println("Задача 4");
     }
-}
+    }
+
+
+
